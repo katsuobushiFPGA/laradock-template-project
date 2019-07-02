50bae3e15f03935bebb9b0c5b009c693492b3f73
horiba <suizlet@gmail.com>
2019-07-02 21:42:15 +0900

    add vue-router and routes settings

--- laravel-project/resources/js/app.js
+++ laravel-project/resources/js/app.js
@@ -5,13 +5,21 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
 window.Vue = require('vue');
 
 // Vuetify
 import Vuetify from 'vuetify';
 import colors from 'vuetify/es5/util/colors';
+import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
+})
 
 Vue.use(Vuetify, {
   theme: {
@@ -23,26 +31,9 @@ Vue.use(Vuetify, {
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router, // ルーティングの定義を読み込む
+    components: { App }, // ルートコンポーネントの使用を宣言する
+    template: '<App />' // ルートコンポーネントを描画する
 });
