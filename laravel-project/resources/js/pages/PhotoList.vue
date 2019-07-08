a03575fa5563e7fd23ca066317736cc611068740
horiba <suizlet@gmail.com>
2019-07-08 21:53:34 +0900

    add sample code

--- laravel-project/resources/js/pages/PhotoList.vue
+++ laravel-project/resources/js/pages/PhotoList.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Photo List</h1>
+  <div>
+    <h1>Photo List1</h1>
+    <button @click="test">test!!</button>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      text: 'example'
+    }
+  },
+  methods : {
+    test () {
+      alert("test!")
+    }
+  }
+}
+</script>
